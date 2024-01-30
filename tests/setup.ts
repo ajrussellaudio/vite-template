25a187b3d674c880f7188115853737c39693936b
Alan Russell <ajrussellaudio@gmail.com>
2024-01-30 20:38:29 +0000

    Install and configure MSW

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,15 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, beforeAll, afterAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import { server } from "./mocks/server";
 
 expect.extend(matchers);
 
+beforeAll(() => server.listen());
+
 afterEach(() => {
+  server.resetHandlers();
   cleanup();
 });
+
+afterAll(() => server.close());
